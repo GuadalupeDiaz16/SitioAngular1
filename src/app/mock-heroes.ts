6a58f8ed76713491c8ec0430fa46eca9445c5634
Guadalupe Díaz <201820250@tese.edu.mx>
2021-03-23 13:24:12 -0600

    Practica

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero}from "./Hero";
+export const HEROES: Hero[]= [
+    {id: 11, name: "Superman", edad: 30},
+    {id: 12, name: "Batman", edad: 30},
+    {id: 13, name: "Hulk", edad: 30},
+    {id: 14, name: "Ironman", edad: 30},
+    {id: 15, name: "Thor", edad: 1000},
+    {id: 16, name: "Spiderman", edad: 20},
+    {id: 17, name: "Black Widow", edad: 28},
+    {id: 18, name: "Flash", edad: 30},
+    {id: 19, name: "Bruja Escarlata", edad: 30},
+    {id: 20, name: "Wonder Woman", edad: 30}
+];
